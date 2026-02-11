551b97f0382d1a260a2631f6ed2824e91d48818d
Ali Saberi <alisaberi@Ali-MacBook-Air.local>
2026-02-11 14:56:27 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+# Arpotaan kolmenumeroinen koodi (jokainen numero väliltä 0-9)
+koodi1_1 = random.randint(0, 9)
+koodi1_2 = random.randint(0, 9)
+koodi1_3 = random.randint(0, 9)
+
+
+
+# Tulostetaan koodit
+print(f"Kolmenumeroinen koodi: {koodi1_1}{koodi1_2}{koodi1_3}")
+
+
---
+++ teh 2.py
@@ -0,0 +1,13 @@
+
+luku = int(input("Anna luku: "))
+
+
+if luku < 0:
+
+    tulos = luku * -1
+else:
+
+    tulos = luku
+
+
+print(f"Luvun itseisarvo on {tulos}")
